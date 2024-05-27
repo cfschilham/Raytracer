acb573fad5b1b5d2bc44161756cad4c6d382a867
Camiel Schilham <cfschilham@protonmail.com>
2024-05-27 15:43:50 +0200

    improve debugger, fix bug in imagetexture causing index out of bounds

--- Raytracer.cs
+++ Raytracer.cs
@@ -11,7 +11,7 @@ public class Raytracer(Surface screen, Camera camera, Scene scene, Vector2i rend
     
     static ThreadLocal<Random> _rand = new (() => new Random(Environment.TickCount));
     
-    const int DebugSize = 20;
+    const float DebugSize = 20;
     private const float DebugMargin = DebugSize * 0.002f;
 
     public static Vector3 RandomUnit() =>
@@ -22,136 +22,147 @@ public class Raytracer(Surface screen, Camera camera, Scene scene, Vector2i rend
     public void Render()
     {
         if (RenderResolution.Y != Screen.Height) throw new Exception("Render resolution must match screen height");
-        List<Ray> debugRays = new();
+        List<(Ray, Intersection?)> debugRays = new();
         Mutex debugRaysMutex = new();
 
         Parallel.For(0, RenderResolution.Y, y => {
             for (int x = 0; x < RenderResolution.X; x++) {
                 Ray ray = Camera.GetRay(x, y);
-                Color color = TraceColor(ray, 5, Array.Empty<Primitive>());
+                Color color;
+                if (y == RenderResolution.Y / 2 && x % 50 == 0) { // Debug ray, set list to non-null
+                    debugRaysMutex.WaitOne();
+                    color = TraceColor(ray, 5, Array.Empty<Primitive>(), debugRays);
+                    debugRaysMutex.ReleaseMutex();
+                }
+                else color = TraceColor(ray, 5, Array.Empty<Primitive>());
                 Screen.Plot(x, y, color.ToInt());
-
-                if (y != RenderResolution.Y / 2 || x % 50 != 0) continue;
-                debugRaysMutex.WaitOne();
-                debugRays.Add(ray);
-                debugRaysMutex.ReleaseMutex();
             }
         });
+        
+        // Draw debug window, start by clearing.
+        for (int y = 0; y < RenderResolution.Y; y++)
+            for (int x = RenderResolution.X; x < Screen.Width; x++)
+                Screen.Plot(x, y, Color.Black.ToInt());
+        
+        // Draw spheres.
+        foreach (Sphere sphere in Scene.Primitives.Where(p => p is Sphere).Cast<Sphere>()) {
+            PlotCircle(GetDebugPixelPoint(sphere.Center.Xz), sphere.Radius, 20);
+        }
+        
+        // Draw camera point.
+        Vector2i cameraPoint = GetDebugPixelPoint(Camera.Position.Xz);
+        Screen.Bar(cameraPoint.X - 1, cameraPoint.Y - 1, cameraPoint.X + 1, cameraPoint.Y + 1, Color.Red.ToInt());
+        
+        // Draw screen plane.
+        Vector2i screenPlaneLeft = GetDebugPixelPoint((Camera.Position + Camera.Target * Camera.FocalLength + Camera.Right * -0.5f * Camera.ScreenPlaneWidth).Xz);
+        Vector2i screenPlaneRight = GetDebugPixelPoint((Camera.Position + Camera.Target * Camera.FocalLength + Camera.Right * 0.5f * Camera.ScreenPlaneWidth).Xz);
+        Screen.Line(screenPlaneLeft.X, screenPlaneLeft.Y, screenPlaneRight.X, screenPlaneRight.Y, Color.Green.ToInt());
 
-        // Debug window
-        Parallel.For(0, RenderResolution.Y, y => {
-            for (int x = RenderResolution.X; x < Screen.Width; x++) {
-                // Convert pixel coordinates to world coordinates, debug window is 10x10.
-                Vector2 point = GetDebugPoint(x, y);
-                Color debugColor = TraceDebugColor(point, debugRays);
-                Screen.Plot(x, y, debugColor.ToInt());
+        // Draw debug rays.
+        foreach ((Ray, Intersection?) debugRay in debugRays) {
+            Vector2i originPixel = GetDebugPixelPoint(debugRay.Item1.Origin.Xz);
+            if (!IsPixelInDebugBounds(originPixel)) continue;
+            
+            Vector2i hitPixel = Vector2i.Zero;
+            float t = debugRay.Item2 == null ? DebugSize : 0;
+            // if (debugRay.Item2 != null) continue;
+            while (!IsPixelInDebugBounds(hitPixel))
+            {
+                if (debugRay.Item2 == null) 
+                    hitPixel = GetDebugPixelPoint(debugRay.Item1.Origin.Xz + t * debugRay.Item1.Direction.Xz);
+                else hitPixel = GetDebugPixelPoint(debugRay.Item2.Value.Point.Xz + t * debugRay.Item1.Direction.Xz);
+                t -= 0.01f;
             }
-        });
+            Screen.Line(originPixel.X, originPixel.Y, hitPixel.X, hitPixel.Y, Color.Blue.ToInt());
+        }
+    }
 
+    void PlotCircle(Vector2i point, float radius, int thickness)
+    {
+        int radiusPixels = (int)(radius * RenderResolution.Y / DebugSize);
+        
+        for (int x = -radiusPixels; x <= radiusPixels; x++) {
+            if (x + point.X < 0 || x + point.X >= Screen.Width) continue;
+            for (int y = -radiusPixels; y <= radiusPixels; y++) {
+                if (y + point.Y < 0 || y + point.Y >= Screen.Height) continue;
+                
+                int r2 = radiusPixels * radiusPixels;
+                if (x * x + y * y < r2 + thickness && x * x + y * y > r2 - thickness) 
+                    Screen.Plot(x + point.X, y + point.Y, Color.White.ToInt());
+            }
+        }
     }
     
-
-    Vector2 GetDebugPoint(int x, int y)
+    bool IsPixelInDebugBounds(Vector2i pixel) =>
+        pixel.X >= RenderResolution.X && pixel.X < Screen.Width && pixel.Y >= 0 && pixel.Y < RenderResolution.Y;
+    
+    Vector2i GetDebugPixelPoint(Vector2 point) => GetDebugPixelPoint(point.X, point.Y);
+    
+    Vector2i GetDebugPixelPoint(float x, float y)
     {
-        float xt = DebugSize * (float)(x - RenderResolution.X) / (Screen.Width - RenderResolution.X - 1);
-        float yt = DebugSize * (float)y / (RenderResolution.Y - 1);
-        yt = DebugSize - yt; // Flip y-axis
-        yt -= DebugSize / 2f; // Center
-        xt -= DebugSize / 2f; // Center
-                
         float aspect = (Screen.Width - RenderResolution.X) / (float)RenderResolution.Y;
-        xt *= aspect;
-        return new Vector2(xt, yt);
-    }
-
-    Color TraceDebugColor(Vector2 point, List<Ray> debugRays)
-    {
-        // Draw spheres. Spheres are always drawn with full radius and center point vec2(p.x, p.z) where p = vec3 of
-        // sphere center. This means that a sphere is always drawn regardless of height in y direction in 3D.
-        foreach (Sphere sphere in Scene.Primitives.Where(p => p is Sphere).Cast<Sphere>()) {
-            if (sphere.DebugIntersect(point, DebugMargin)) return Color.White;
-        }
-        
-        // Draw the camera point. Just like spheres, camera is always drawn regardless of Y height.
-        if ((point - new Vector2(Camera.Position.X, Camera.Position.Z)).Length < DebugMargin * 2) return Color.Red;
-        
-        // Draw the screen plane.
-        Vector3 screenPlaneLeftPoint = Camera.Position + Camera.Target * Camera.FocalLength + Camera.Right * -0.5f * Camera.ScreenPlaneWidth;
-        float dp = Vector2.Dot((point - screenPlaneLeftPoint.Xz).Normalized(), Camera.Right.Xz.Normalized());
-        float t = Vector2.Dot(point - screenPlaneLeftPoint.Xz, Camera.Right.Xz.Normalized());
-        if (dp < 1 + DebugMargin * (1/(t*40)) && dp > 1 - DebugMargin * (1/(t*40))) {
-            if (t > 0 && t < Camera.ScreenPlaneWidth) return Color.Green;
-        }
-        
-        // Draw debug rays
-        foreach (Ray debugRay in debugRays) {
-            Intersection? intsect = Trace(debugRay);
-            
-            dp = Vector2.Dot((point - debugRay.Origin.Xz).Normalized(), debugRay.Direction.Xz.Normalized());
-            float rayLength = (debugRay.Origin.Xz - point).Length;
-            if (dp > 1 + DebugMargin * (0.01/rayLength) || dp < 1 - DebugMargin * (0.01/rayLength)) continue;
-            if (intsect == null) return Color.Blue;
-            
-            float intsectDistance = (intsect.Value.Point.Xz - debugRay.Origin.Xz).Length;
-            t = Vector2.Dot(point - debugRay.Origin.Xz, debugRay.Direction.Xz.Normalized());
-            if (t > 0 && t < intsectDistance) return Color.Blue;
-        }
-        
-        return Color.Black;
+        x += DebugSize * aspect / 2;
+        y += DebugSize / 2;
+        x *= (Screen.Width - RenderResolution.X) / (DebugSize * aspect);
+        y *= RenderResolution.Y / DebugSize;
+        y = RenderResolution.Y - y; // Flip y-axis
+        // x *= (Screen.Width - RenderResolution.X) / (float)Screen.Width; // Adjust for aspect ratio
+        return new Vector2i((int)x + RenderResolution.X, (int)y);
     }
 
     Intersection? Trace(Ray ray) {
         Intersection? closest = null;
         foreach (Primitive primitive in Scene.Primitives) {
-            Intersection? intersection = primitive.Intersect(ray);
-            if (intersection != null && (closest == null || intersection.Value.Distance < closest.Value.Distance)) {
-                closest = intersection;
+            Intersection? hit = primitive.Intersect(ray);
+            if (hit != null && (closest == null || hit.Value.Distance < closest.Value.Distance)) {
+                closest = hit;
             }
         }
 
         return closest;
     }
     
-    Color TraceColor(Ray ray, int depth, Primitive[] ignore) {
+    Color TraceColor(Ray ray, int depth, Primitive[] ignore, List<(Ray, Intersection?)>? debugRays = null) {
         if (depth == 0) return Color.Black;
         
-        Intersection? intsect = Trace(ray);
-        if (intsect == null) return Color.SkyBlue;
-        Primitive prim = intsect.Value.Primitive;
+        Intersection? hit = Trace(ray);
+        if (debugRays != null) debugRays.Add((ray, hit));
+        if (hit == null) return Color.SkyBlue;
+        Primitive prim = hit.Value.Primitive;
         if (ignore.Contains(prim)) return Color.Black;
 
         Color specular = Color.Black;
         Color diffuse = Color.Black;
         foreach (PointLight light in Scene.Lights.Where(l => l is PointLight).Cast<PointLight>())
         {
-            Vector3 lightDir = Vector3.Normalize(light.Position - intsect.Value.Point);
+            Vector3 lightDir = Vector3.Normalize(light.Position - hit.Value.Point);
             
             // Attenuation factor for light intensity scales with the distance to the light. Uses inverse square law.
-            float attenuation = 1 / ((light.Position - intsect.Value.Point).LengthSquared * 0.03f);
+            float attenuation = 1 / ((light.Position - hit.Value.Point).LengthSquared * 0.03f);
             
             // If there is an object between the current intersection and the light, the current intersection is in
             // shadow. Add a small offset to the point to avoid self-intersection.
-            Intersection? shadowIntsect = Trace(new Ray(intsect.Value.Point + 0.05f * lightDir, lightDir));
-            if (shadowIntsect != null && shadowIntsect.Value.Distance < (light.Position - intsect.Value.Point).Length)
+            Intersection? shadowHit = Trace(new Ray(hit.Value.Point + 0.05f * lightDir, lightDir));
+            if (shadowHit != null && shadowHit.Value.Distance < (light.Position - hit.Value.Point).Length)
                 continue;
             
             // Angle factor for diffuse lighting scales with the angle of the light relative to the normal. Dot product
             // 0 is perpendicular, thus no light, 1 is parallel, thus full light.
-            float angleFactor = Vector3.Dot(lightDir, intsect.Value.Normal);
-            diffuse += prim.Material.Diffuse.Pixel(intsect.Value.UV.X, intsect.Value.UV.Y) * light.Color * Math.Max(angleFactor, 0) * attenuation;
+            float angleFactor = Vector3.Dot(lightDir, hit.Value.Normal);
+            diffuse += prim.Material.Diffuse.Pixel(hit.Value.UV.X, hit.Value.UV.Y) * light.Color * Math.Max(angleFactor, 0) * attenuation;
             
             // Specular lighting
-            Vector3 reflectDir = Reflect(-lightDir, intsect.Value.Normal);
-            Vector3 viewDir = Vector3.Normalize(ray.Origin - intsect.Value.Point);
+            Vector3 reflectDir = Reflect(-lightDir, hit.Value.Normal);
+            Vector3 viewDir = Vector3.Normalize(ray.Origin - hit.Value.Point);
             float specAngle = Vector3.Dot(reflectDir, viewDir);
-            specular += prim.Material.Specular.Pixel(intsect.Value.UV.X, intsect.Value.UV.Y) * light.Color * MathF.Pow(Math.Max(specAngle, 0), prim.Material.Shininess) * attenuation;
+            specular += prim.Material.Specular.Pixel(hit.Value.UV.X, hit.Value.UV.Y) * light.Color * MathF.Pow(Math.Max(specAngle, 0), prim.Material.Shininess) * attenuation;
         }
         
         if (prim.Material.Reflectivity > 0) {
-            Vector3 reflectDir = Reflect(ray.Direction, intsect.Value.Normal);
-            Ray reflectRay = new Ray(intsect.Value.Point + 0.05f * reflectDir, reflectDir);
+            Vector3 reflectDir = Reflect(ray.Direction, hit.Value.Normal);
+            Ray reflectRay = new Ray(hit.Value.Point + 0.05f * reflectDir, reflectDir);
 
-            Color reflectColor = TraceColor(reflectRay, depth - 1, ignore.Concat(new []{prim}).ToArray());
+            Color reflectColor = TraceColor(reflectRay, depth - 1, ignore.Concat(new []{prim}).ToArray(), debugRays);
             
             if (prim.Material.Gloss > 0)
             {
@@ -161,14 +172,16 @@ public class Raytracer(Surface screen, Camera camera, Scene scene, Vector2i rend
                     glossRay.Direction += prim.Material.Gloss * RandomUnit();
                     while (Vector3.Dot(glossRay.Direction, reflectRay.Direction) <= 0.1f) 
                         glossRay.Direction = reflectRay.Direction + prim.Material.Gloss * RandomUnit();
-                    glossColors[i] = TraceColor(glossRay, depth - 1, ignore.Concat(new []{prim}).ToArray());
+                    if (i%50 == 0) 
+                        glossColors[i] = TraceColor(glossRay, depth - 1, ignore.Concat(new []{prim}).ToArray(), debugRays);
+                    else glossColors[i] = TraceColor(glossRay, depth - 1, ignore.Concat(new []{prim}).ToArray());
                 }
                 reflectColor = Color.Avg(glossColors);
             }
             specular += reflectColor * prim.Material.Reflectivity; 
         }
 
-        Color ambient = prim.Material.Ambient.Pixel(intsect.Value.UV.X, intsect.Value.UV.Y) * Light.Ambient;
+        Color ambient = prim.Material.Ambient.Pixel(hit.Value.UV.X, hit.Value.UV.Y) * Light.Ambient;
         return (ambient + diffuse + specular) * prim.Material.Gamma;
     }
 }
